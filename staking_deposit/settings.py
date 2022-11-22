b1b49feb1179d66ab5d4df92b9caa7daec917bab
Shane Earley <shanejearley@gmail.com>
2022-11-21 21:22:48 -0500

    Add devnet

--- staking_deposit/settings.py
+++ staking_deposit/settings.py
@@ -15,6 +15,7 @@ GOERLI = 'goerli'
 PRATER = 'prater'
 KILN = 'kiln'
 SEPOLIA = 'sepolia'
+DEVNET = 'devnet'
 
 
 # Mainnet setting
@@ -27,6 +28,8 @@ GoerliSetting = BaseChainSetting(NETWORK_NAME=GOERLI, GENESIS_FORK_VERSION=bytes
 KilnSetting = BaseChainSetting(NETWORK_NAME=KILN, GENESIS_FORK_VERSION=bytes.fromhex('70000069'))
 # Sepolia setting
 SepoliaSetting = BaseChainSetting(NETWORK_NAME=SEPOLIA, GENESIS_FORK_VERSION=bytes.fromhex('90000069'))
+# Devnet setting
+DevnetSetting = BaseChainSetting(NETWORK_NAME=DEVNET, GENESIS_FORK_VERSION=bytes.fromhex('10000038'))
 
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
@@ -36,6 +39,7 @@ ALL_CHAINS: Dict[str, BaseChainSetting] = {
     PRATER: GoerliSetting,  # Prater is the old name of the Prater/Goerli testnet
     KILN: KilnSetting,
     SEPOLIA: SepoliaSetting,
+    DEVNET: DevnetSetting
 }
 
 
